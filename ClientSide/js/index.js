051fb59ecf44862ada37a0eb554c55b65d4bf876
avally0611 <avally7860@gmail.com>
2025-03-17 21:44:31 +0200

    Update index.js

--- ClientSide/js/index.js
+++ ClientSide/js/index.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() 
-{
-    const startButton = document.getElementById('start');
 
-    startButton.addEventListener('click', function(){
-        window.location.href = 'types.html';
-    });
-});
